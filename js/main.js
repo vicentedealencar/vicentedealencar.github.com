706fec3529ac85128872fb6cb41908dd55856294
Vicente de Alencar <vicentedealencar@gmail.com>
2012-10-31 11:29:10 -0200

    base isotope

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(function(){      
+    var $container = $('.isotope');
 
+    $container.isotope({
+        itemSelector: '.isotope-item'
+    });
+
+    // filter items when filter link is clicked
+    $('#isotope-filters a').click(function () {
+        var selector = $(this).attr('data-filter');
+        $container.isotope({ filter: selector });
+        return false;
+    });
+});
