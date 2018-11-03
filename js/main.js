a019f0ba8d9633d5839b7f8d7b6b0e717bb74c2d
Vicente de Alencar <vicentedealencar@gmail.com>
2018-11-02 22:14:08 -0300

    remove all bloat

--- js/main.js
+++ js/main.js
@@ -1,33 +1,6 @@
 $(function () {
     $("#title").fitText();
 
-    var $container = $('.isotope');
-    $container.isotope({
-        itemSelector: '.isotope-item'
-    });
-
-    // filter items when filter link is clicked
-    $('#isotope-filters a').click(function () {
-        var selector = $(this).attr('data-filter');
-        $container.isotope({ filter: selector });
-        return false;
-    });
-
-    var contatoForm = $('#contato-form');
-    contatoForm.on('submit', function (e) {
-        $.post('https://zapier.com/hooks/catch/n/2PAB/',
-        {
-            subject: contatoForm.find('[name=subject]').val(),
-            body: contatoForm.find('[name=body]').val()
-        })
-        .complete( function (data) {
-            alert('Obrigado!');
-        });
-
-        e.preventDefault();
-        return false;
-    });
-
     analytics.initialize({
     'Google Analytics' : 'UA-35941179-1'
     });
